c95337040f90a5266207e489592b404f957f00a5
Bradley Powell <bradley.powell18@gmail.com>
2022-09-07 13:49:43 -0500

    Array Uploads

---
+++ Arrays/PrimeNumbers/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+
+    // Initialize the necessary variables
+    int low = 3;
+    int high = 100;
+
+    int
+
+    // Create a for-loop 3 to 100 and iterate for each
+
+        // Do a calculation
+
+        // If the number is deemed a prime
+            //Store
+
+        //else
+            //drop that shiz
+
+
+
+
+    return 0;
+}
